ed7bc36290fd4c10ed77b16a3d3b3aa2aa3e5a2b
Trdngx <hatridung1610@gmail.com>
2023-04-20 21:53:16 +0700

    Product

---
+++ ProductManagement/ProductManagement/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace WinForm_EF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtImage = new System.Windows.Forms.TextBox();
+            this.txtUnit = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.nbrUnitInStock = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.nbrUnitInStock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(232, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Management";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ProductID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(399, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ProductName";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 275);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "UnitInStock";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(58, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "UnitPrice";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(399, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Image";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(399, 280);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Category";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(197, 102);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(125, 27);
+            this.txtId.TabIndex = 7;
+            // 
+            // txtImage
+            // 
+            this.txtImage.Location = new System.Drawing.Point(558, 184);
+            this.txtImage.Name = "txtImage";
+            this.txtImage.Size = new System.Drawing.Size(125, 27);
+            this.txtImage.TabIndex = 10;
+            // 
+            // txtUnit
+            // 
+            this.txtUnit.Location = new System.Drawing.Point(197, 181);
+            this.txtUnit.Name = "txtUnit";
+            this.txtUnit.Size = new System.Drawing.Size(125, 27);
+            this.txtUnit.TabIndex = 11;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(558, 99);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(125, 27);
+            this.txtName.TabIndex = 12;
+            // 
+            // nbrUnitInStock
+            // 
+            this.nbrUnitInStock.Location = new System.Drawing.Point(197, 273);
+            this.nbrUnitInStock.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nbrUnitInStock.Name = "nbrUnitInStock";
+            this.nbrUnitInStock.Size = new System.Drawing.Size(150, 27);
+            this.nbrUnitInStock.TabIndex = 13;
+            this.nbrUnitInStock.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(532, 277);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(48, 358);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(94, 29);
+            this.btnInsert.TabIndex = 15;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(281, 358);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(94, 29);
+            this.btnUpdate.TabIndex = 16;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(558, 358);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(94, 29);
+            this.btnDelete.TabIndex = 17;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 450);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(810, 252);
+            this.dataGridView1.TabIndex = 18;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 736);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.nbrUnitInStock);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtUnit);
+            this.Controls.Add(this.txtImage);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nbrUnitInStock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtImage;
+        private System.Windows.Forms.TextBox txtUnit;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.NumericUpDown nbrUnitInStock;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ ProductManagement/ProductManagement/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinForm_EF.Models;
+
+namespace WinForm_EF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadData();
+
+        }
+
+        private void loadData()
+        {
+            using (MySaleDB1Context context = new MySaleDB1Context())
+            {
+                var data1 = context.Products.Select(item => new 
+                { 
+                    ProductID=item.ProductId,
+                    ProductName=item.ProductName,
+                    Price=item.UnitPrice,
+                    Stock=item.UnitsInStock,
+                    Image=item.Image,
+                    CategoryName=item.Category.CategoryName
+                }).ToList();
+                dataGridView1.DataSource = data1;
+
+                //var data2 = context.Categories.ToList();
+                //comboBox1.DataSource = data2;
+                //comboBox1.DisplayMember = "CategoryName";
+                //comboBox1.ValueMember = "CategoryId";
+            }
+
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtId.Text = dataGridView1.Rows[e.RowIndex].Cells[0].FormattedValue.ToString();
+            txtName.Text = dataGridView1.Rows[e.RowIndex].Cells[1].FormattedValue.ToString();
+            txtUnit.Text = dataGridView1.Rows[e.RowIndex].Cells[2].FormattedValue.ToString();
+            
+            nbrUnitInStock.Value = Convert.ToUInt32 (dataGridView1.Rows[e.RowIndex].Cells[3].FormattedValue.ToString());
+            txtImage.Text = dataGridView1.Rows[e.RowIndex].Cells[4].FormattedValue.ToString();
+            comboBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[5].FormattedValue.ToString();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            using (MySaleDB1Context context = new MySaleDB1Context())
+            {
+                // Tao doi tuong se insert
+                Product product = new Product {
+                
+                    ProductName=txtName.Text,
+                    UnitPrice=Convert.ToDecimal(txtUnit.Text),
+                    UnitsInStock=Convert.ToInt32(nbrUnitInStock.Value),
+                    Image=txtImage.Text,
+                    CategoryId=Convert.ToInt32(comboBox1.SelectedValue)
+                };
+                context.Products.Add(product);
+                if (context.SaveChanges()>0)
+                {
+                    MessageBox.Show("Insert successfully");
+                    loadData();
+                }
+
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (MySaleDB1Context context = new MySaleDB1Context())
+            {
+                // Tim doi tuong can xoa
+                Product product = context.Products.SingleOrDefault(item => item.ProductId == Convert.ToInt32(txtId.Text));
+                
+                context.Products.Remove(product);
+                if (context.SaveChanges() > 0)
+                {
+                    MessageBox.Show("Delete successfully");
+                    loadData();
+                }
+
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using (MySaleDB1Context context = new MySaleDB1Context())
+            {
+                //Tim product muon update
+                Product product = context.Products.SingleOrDefault(item => item.ProductId == Convert.ToInt32(txtId.Text));
+
+                //setting lai nhung gia tri muon update
+                product.ProductName = txtName.Text;
+                product.UnitPrice = Convert.ToDecimal(txtUnit.Text);
+                product.UnitsInStock = Convert.ToInt32(nbrUnitInStock.Text);
+                product.Image = txtImage.Text;
+                product.CategoryId = Convert.ToInt32(comboBox1.SelectedValue);
+
+                
+                if (context.SaveChanges() > 0)
+                {
+                    MessageBox.Show("update successfully");
+                    loadData();
+                }
+
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
